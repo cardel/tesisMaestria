eba522112480d800c22f2590c84a9078a6ee3f7a
Carlos Delgado <cardel@debian-portatil>
2017-12-03 16:56:09 -0500

    Avance 3 de Diciembre

--- implementacion/TrabajoGrado/3.GreedingColoringFractality/greedingColoring.cpp
+++ implementacion/TrabajoGrado/3.GreedingColoringFractality/greedingColoring.cpp
@@ -6,13 +6,33 @@
 #include <algorithm> 
 using std::set;
 using std::min_element;
+using std::max_element;
+using std::advance;
 
 
 int main(int argc, char* argv[]) {
 	
+	srand(NULL);
+	//Parametros
+	char* typeNet="";
+	char* path="";
+	for(int i=1; i<argc; i++){
+		if (strcmp(argv[i],"--file")==0){
+			path=argv[i+1];
+		}
+		if(strcmp(argv[i],"--type")==0){
+			typeNet=argv[i+1];
+		}
+	}
 	//typedef PUNGraph PGraph; //Grafo no dirigido
-	//PGraph G2 = TSnap::LoadPajek<PGraph>(argv[1]);
-    PUNGraph G2 = TSnap::LoadEdgeList<PUNGraph>(argv[1], 0, 1);
+	PUNGraph G2;
+	if(strcmp(typeNet,"edge")==0){
+		G2 = TSnap::LoadEdgeList<PUNGraph>(path, 0, 1);
+	}
+	else{
+		G2 = TSnap::LoadPajek<PUNGraph>(path);
+		
+	}
 	//.GenPrefAttach(30, 3, snap.TRnd())	
 	
 	//Get lbmax (max distance in the network)
@@ -26,47 +46,64 @@ int main(int argc, char* argv[]) {
 	for (TUNGraph::TNodeI NI = G2->BegNI(); NI < G2->EndNI(); NI++){
 		listaID.Add(NI.GetId());
 	}
-	
+
 	//Colores
 	int colorForNodeNbyLB[numNodes][lbMax];
 	
-	//Rellenar
-	for(int i=0; i<numNodes; i++){
-		for(int j=0; j<lbMax;j++){
-			colorForNodeNbyLB[0][i]=-1;
-		}
-	}
 	//Para el ID = 0, el color es 0
 	for(int i=0; i<lbMax;i++){
 		colorForNodeNbyLB[0][i]=0;
 	}
 	printf("%s%i\n","LbMax ",lbMax);
-    //Create colors
 
-    
-    //Computational complex: O(n^2*b^n) --> Exponential
+	//When the distance is lbMax all nodes have the same color
+	/*for(int i=1; i<numNodes;i++){
+		colorForNodeNbyLB[i][lbMax-1]=0;
+	}*/		
+
+	int maxColor = 0;
 	for(int i=1; i<listaID.Len(); i++){	 //n (number of nodes)
 		//printf("%s%i\n","Nodo: ",i);
 		int distanceij[i];
-        //Cost is n(n+1)/2
-        
+		
+        //Cost is n(n+1)/2 * O(b^d)        
 		for(int j=0; j<i; j++){
             //Cost in worst case for each BFS execution O(b^d) where d is distance and b is factor
-			distanceij[j]=TSnap::GetShortPath(G2,listaID[i],listaID[j]);
+			distanceij[j]=TSnap::GetShortPath(G2,listaID[j],listaID[i]);
 		}
+		
 		//I suppose to lb is n (worst case)
-		//Distance is form 1 until lbMax
+		//Distance is from 2 until lbMax, remember, lbMax is now lbMax+1
         for(int lb = 1; lb<=lbMax; lb++){
 			//Cost is n(n+1)/2            
-            set <int> usedColors;
-			for(int j=0; j<i; j++){				
+            set <int> nonValidColors;
+            set <int> validColors;
+           
+			for(int j=0; j<i; j++){	
 				if(distanceij[j]>=lb){
-                    usedColors.insert(colorForNodeNbyLB[j][distanceij[j]]);
+                    nonValidColors.insert(colorForNodeNbyLB[j][lb-1]);
+				}
+				else{
+					validColors.insert(colorForNodeNbyLB[j][lb-1]);
 				}
 			}
-            //An unusedColor is not an element of the set of color cjlij
-            int unusedColor = *max_element(usedColors.begin(),usedColors.end()) + 1;
-            colorForNodeNbyLB[i][lb]=unusedColor;
+            set<int> possibleColors;
+            set_difference(validColors.begin(),validColors.end(),nonValidColors.begin(),nonValidColors.end(),std::inserter(possibleColors,possibleColors.begin()));
+            if(possibleColors.size()>0){
+				int randomPosition = rand() % possibleColors.size();  
+				
+				set<int>::const_iterator it(possibleColors.begin());
+				advance(it,randomPosition);
+				
+				colorForNodeNbyLB[i][lb-1]=*(it);
+            }
+            else{
+				set<int> unionColors;
+				set_union(nonValidColors.begin(),nonValidColors.end(),validColors.begin(),validColors.end(),std::inserter(unionColors,unionColors.begin()));
+				
+				colorForNodeNbyLB[i][lb-1] = *max_element(unionColors.begin(),unionColors.end())+1;
+
+			}
 		}
 	}
 
@@ -81,6 +118,12 @@ int main(int argc, char* argv[]) {
 			numColors.insert(colorForNodeNbyLB[i][lb]);
 		}
 		boxes.Add(numColors.size());  		
+	}
+	for(int i=0; i<numNodes; i++){
+		for(int j=0; j<lbMax;j++){
+			printf("%i,",colorForNodeNbyLB[i][j]);
+		}
+		printf("\n");
 	}
     printf("%s\t%s\n","Lb","Boxes");
 	for(int i=0; i<boxes.Len(); i++){
