73403317672b9cbf241a478b6b98e822eed60b50
Carlos Delgado <cardel@debian-portatil>
2018-04-22 02:12:54 -0500

    Tests

--- implementacion/lamfria/pruebasFractalidad.sh
+++ implementacion/lamfria/pruebasFractalidad.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-python pruebasFractalidad.py --file ../datos/football.net --type Pajek --output footballDegree --message "football"  &
+python pruebasFractalidad.py --file ../datos/football.net --type Pajek --output footballDegree --message "football"  
 
-python pruebasFractalidad.py --file ../datos/karate.net --type Pajek --output karateDegree --message "karate"  &
+python pruebasFractalidad.py --file ../datos/karate.net --type Pajek --output karateDegree --message "karate"  
 
-python pruebasFractalidad.py --file ../datos/cellular.dat --type Edge --output cellularDegree --message "cellular"  &
+python pruebasFractalidad.py --file ../datos/cellular.dat --type Edge --output cellularDegree --message "cellular"  
 
 #(2,2)-flower 7th generation
 
-python pruebasFractalidad.py --type Flower --output FlowerDegree --message "flower" &
+python pruebasFractalidad.py --type Flower --output FlowerDegree --message "flower" 
 
 #Scalefree 3000 nodos, 100 grado de salida
 
@@ -17,7 +17,7 @@ for Nodes in 300 500 1000
 do
 	for Edges in 1200 2000
 	do
-		python pruebasFractalidad.py --type SmallWorld --output "SmallWorld"$Nodes"d"$Edges --message "SmallWorld"$Nodes"d"$Edges --node $Nodes --desired $Edges &
+		python pruebasFractalidad.py --type SmallWorld --output "SmallWorld"$Nodes"d"$Edges --message "SmallWorld"$Nodes"d"$Edges --node $Nodes --desired $Edges 
 	done
 done
 
@@ -26,7 +26,7 @@ for Nodes in 300 500 1000
 do
 	for Edges in 1200 2000
 	do
-		python pruebasFractalidad.py --type ScaleFreePowerLaw --output "ScaelFreePowerLaw"$Nodes"d"$Edges --message "ScaleFreePowerLaw"$Nodes"d"$Edges --node $Nodes --desired $Edges  &
+		python pruebasFractalidad.py --type ScaleFreePowerLaw --output "ScaelFreePowerLaw"$Nodes"d"$Edges --message "ScaleFreePowerLaw"$Nodes"d"$Edges --node $Nodes --desired $Edges  
 	done
 done
 
@@ -35,6 +35,6 @@ for Nodes in 300 500 1000
 do
 	for Edges in 1200 2000
 	do
-		python pruebasFractalidad.py --type Random --output "Random"$Nodes"d"$Edges --message "Random"$Nodes"d"$Edges --node $Nodes --desired $Edges  &
+		python pruebasFractalidad.py --type Random --output "Random"$Nodes"d"$Edges --message "Random"$Nodes"d"$Edges --node $Nodes --desired $Edges  
 	done
 done
