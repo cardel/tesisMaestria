c038bb87a13fb05c43638f2bf9f4b8afde62352f
Carlos Delgado <cardel@debian-portatil>
2018-04-24 01:35:39 -0500

    tests

--- implementacion/lamfria/tests/test1.sh
+++ implementacion/lamfria/tests/test1.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+cd ..
 python pruebasFractalidad.py --type Random --output Random50d100 --message Random50d100 --node 1000 --desired 20000 
--- implementacion/lamfria/tests/test2.sh
+++ implementacion/lamfria/tests/test2.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+cd ..
 python pruebasFractalidad.py --type ScaleFreePrefAttach --output ScaleFreePrefAttach --message ScaleFreePrefAttach1000-5000 --node 1000 --desired 20000 
