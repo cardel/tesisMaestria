10598e0cf3cf4a2536475e6407ebf7af872623db
Carlos Delgado <cardel@debian-portatil>
2018-04-25 16:35:51 -0500

    Tests2

---
+++ implementacion/lamfria/pruebasSimposio2.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+python pruebasRobustez.py --type SmallWorld --output SmallWorldGC --message SmallWorldGC --node 100 --desired 500 --measure GC --attack random&
+python pruebasRobustez.py --type ScaleFreePrefAttach --output ScaleFreePrefAttachMeasureGC --message ScaleFreePrefAttachMeasureGC --node 100 --desired 500  --measure GC --attack random&
+python pruebasRobustez.py --type Random --output RandomGC --message RandomGC --node 100 --desired 500 --measure GC --attack random &
