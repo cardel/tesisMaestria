672da84da10cfceb08f0ac214e48806f06d14e58
Carlos Delgado <cardel@debian-portatil>
2018-04-29 22:05:22 -0500

    Changes

--- implementacion/lamfria/tests.sh
+++ implementacion/lamfria/tests.sh
@@ -1,49 +1,59 @@
 #!/bin/sh
 #!/bin/sh
-python pruebasFractalidad.py --file ../datos/Generadas/ScaleFree2000Nodes.txt --type Edge  --output ScaleFree2000Nodes
-python pruebasFractalidad.py --file ../datos/Generadas/ScaleFree4000Nodes.txt --type Edge  --output ScaleFree4000Nodes
-python pruebasFractalidad.py --file ../datos/Generadas/ScaleFree8000Nodes.txt --type Edge  --output ScaleFree8000Nodes
 
-#!/bin/sh
+#Maquina 1 
+python pruebasFractalidad.py --file ../datos/Generadas/ScaleFree2000Nodes.txt --type Edge  --output ScaleFree2000Nodes  
+python pruebasFractalidad.py --file ../datos/Generadas/ScaleFree4000Nodes.txt --type Edge  --output ScaleFree4000Nodes 
+
+#Maquina 2
+python pruebasFractalidad.py --file ../datos/Generadas/ScaleFree8000Nodes.txt --type Edge  --output ScaleFree8000Nodes 
 python pruebasFractalidad.py --file ../datos/Generadas/SmallWorld5000NodesRewire005.txt --type Edge  --output SmallWorld5000NodesRewire005
-python pruebasFractalidad.py --file ../datos/Generadas/SmallWorld5000NodesRewire01.txt --type Edge  --output SmallWorld5000NodesRewire01
-python pruebasFractalidad.py --file ../datos/Generadas/SmallWorld5000NodesRewire02.txt --type Edge  --output SmallWorld5000NodesRewire02
 
-#!/bin/sh
+#Maquina 3
+python pruebasFractalidad.py --file ../datos/Generadas/SmallWorld5000NodesRewire01.txt --type Edge  --output SmallWorld5000NodesRewire01
+python pruebasFractalidad.py --file ../datos/Generadas/SmallWorld5000NodesRewire02.txt --type Edge  --output SmallWorld5000NodesRewire02  
+
+#Maquina 4
 python pruebasFractalidad.py --file ../datos/Generadas/Random1991Nodes5939.txt --type Edge --output Random1991Nodes5939
 python pruebasFractalidad.py --file ../datos/Generadas/Random3373Nodes5978.txt --type Edge  --output Random3373Nodes5978
+
+#Maquina 5
 python pruebasFractalidad.py --file ../datos/Generadas/Random5620Nodes8804.txt --type Edge  --output Random5620Nodes8804
-
-
-#!/bin/sh
 python pruebasFractalidad.py --file ../datos/RedesReales/Celengs.net  --type Pajek --output Celengs
+
+#Maquina 6
 python pruebasFractalidad.py --file ../datos/RedesReales/cerevisiae.net  --type Pajek --output cerevisiae
 python pruebasFractalidad.py --file ../datos/RedesReales/EColi.net  --type Pajek --output EColi
 
-
-#!/bin/sh
+#Maquina 7
 python pruebasFractalidad.py --file ../datos/Fractales/floweru2v2.net  --type Pajek --output floweru2v2
 python pruebasFractalidad.py --file ../datos/Fractales/floweru1v3.net  --type Pajek --output floweru1v3
 
+#Maquina 8
+python pruebasRobustez.py --file ../datos/Generadas/ScaleFree2000Nodes.txt --type Edge  --output ScaleFree2000Nodes  
+python pruebasRobustez.py --file ../datos/Generadas/ScaleFree4000Nodes.txt --type Edge  --output ScaleFree4000Nodes  
 
+#Maquina 9
+python pruebasRobustez.py --file ../datos/Generadas/ScaleFree8000Nodes.txt --type Edge  --output ScaleFree8000Nodes  
+python pruebasRobustez.py --file ../datos/Generadas/SmallWorld5000NodesRewire005.txt --type Edge  --output SmallWorld5000NodesRewire005 
 
-python pruebasRobustez.py --file ../datos/Generadas/ScaleFree2000Nodes.txt --type Edge  --output ScaleFree2000Nodes  Ejecutando
-python pruebasRobustez.py --file ../datos/Generadas/ScaleFree4000Nodes.txt --type Edge  --output ScaleFree4000Nodes  Ejedutando
-python pruebasRobustez.py --file ../datos/Generadas/ScaleFree8000Nodes.txt --type Edge  --output ScaleFree8000Nodes  Ejecutando
+#Maquina 10
+python pruebasRobustez.py --file ../datos/Generadas/SmallWorld5000NodesRewire01.txt --type Edge  --output SmallWorld5000NodesRewire01 
+python pruebasRobustez.py --file ../datos/Generadas/SmallWorld5000NodesRewire02.txt --type Edge  --output SmallWorld5000NodesRewire02 
 
-python pruebasRobustez.py --file ../datos/Generadas/SmallWorld5000NodesRewire005.txt --type Edge  --output SmallWorld5000NodesRewire005 Ejecutando
-python pruebasRobustez.py --file ../datos/Generadas/SmallWorld5000NodesRewire01.txt --type Edge  --output SmallWorld5000NodesRewire01 Ejedutando
-python pruebasRobustez.py --file ../datos/Generadas/SmallWorld5000NodesRewire02.txt --type Edge  --output SmallWorld5000NodesRewire02 Ejecutando
+#Maquina 11
+python pruebasRobustez.py --file ../datos/Generadas/Random1991Nodes5939.txt --type Edge --output Random1991Nodes5939 
+python pruebasRobustez.py --file ../datos/Generadas/Random3373Nodes5978.txt --type Edge  --output Random3373Nodes5978 
 
-python pruebasRobustez.py --file ../datos/Generadas/Random1991Nodes5939.txt --type Edge --output Random1991Nodes5939 Ejecutando
-python pruebasRobustez.py --file ../datos/Generadas/Random3373Nodes5978.txt --type Edge  --output Random3373Nodes5978 Ejecutando
-python pruebasRobustez.py --file ../datos/Generadas/Random5620Nodes8804.txt --type Edge  --output Random5620Nodes8804 Ejecutando
+#Maquina 12
+python pruebasRobustez.py --file ../datos/Generadas/Random5620Nodes8804.txt --type Edge  --output Random5620Nodes8804 
+python pruebasRobustez.py --file ../datos/RedesReales/Celengs.net  --type Pajek --output Celengs 
 
+#Maquina 13
+python pruebasRobustez.py --file ../datos/RedesReales/cerevisiae.net  --type Pajek --output cerevisiae  
+python pruebasRobustez.py --file ../datos/RedesReales/EColi.net  --type Pajek --output EColi 
 
-python pruebasRobustez.py --file ../datos/RedesReales/Celengs.net  --type Pajek --output Celengs Ejecutando
-python pruebasRobustez.py --file ../datos/RedesReales/cerevisiae.net  --type Pajek --output cerevisiae  Ejecutando
-python pruebasRobustez.py --file ../datos/RedesReales/EColi.net  --type Pajek --output EColi Ejecutando
-
-python pruebasRobustez.py --file ../datos/Fractales/floweru2v2.net  --type Pajek --output floweru2v2 Ejecutando
-python pruebasRobustez.py --file ../datos/Fractales/floweru1v3.net  --type Pajek --output floweru1v3 Ejecutando
+#Maquina 14
+python pruebasRobustez.py --file ../datos/Fractales/floweru2v2.net  --type Pajek --output floweru2v2 
+python pruebasRobustez.py --file ../datos/Fractales/floweru1v3.net  --type Pajek --output floweru1v3 
 
