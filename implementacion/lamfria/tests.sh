9e45c4b994087757d2c1bf861214b15c81d2bb34
Carlos Delgado <cardel@debian-portatil>
2018-05-04 16:25:08 -0500

    Ecoli

--- implementacion/lamfria/tests.sh
+++ implementacion/lamfria/tests.sh
@@ -2,35 +2,35 @@
 #!/bin/sh
 
 #Maquina 1 
-python pruebasFractalidadFaltantes.py --file ../datos/Generadas/ScaleFree2000Nodes.txt --type Edge  --output ScaleFree2000Nodes  Haciendo faltantes
-python pruebasFractalidad.py --file ../datos/Generadas/ScaleFree4000Nodes.txt --type Edge  --output ScaleFree4000Nodes 
+python pruebasFractalidadFaltantes.py --file ../datos/Generadas/ScaleFree2000Nodes.txt --type Edge  --output ScaleFree2000Nodes  COMPLETA
+python pruebasFractalidadFaltantes.py --file ../datos/Generadas/ScaleFree4000Nodes.txt --type Edge  --output ScaleFree4000Nodes  OK  --- Haciendo faltantes (Maquina 1, lado 2)
 
 #Maquina 2
-python pruebasFractalidad.py --file ../datos/Generadas/ScaleFree8000Nodes.txt --type Edge  --output ScaleFree8000Nodes 
-python pruebasFractalidad.py --file ../datos/Generadas/SmallWorld5000NodesRewire005.txt --type Edge  --output SmallWorld5000NodesRewire005
+python pruebasFractalidadFaltantes.py  --file ../datos/Generadas/ScaleFree8000Nodes.txt --type Edge  --output ScaleFree8000Nodes   OK  --- Haciendo faltantes (Maquina 1, lado 2)
+python pruebasFractalidad.py --file ../datos/Generadas/SmallWorld5000NodesRewire005.txt --type Edge  --output SmallWorld5000NodesRewire005  OK
 
 #Maquina 3
-python pruebasFractalidad.py --file ../datos/Generadas/SmallWorld5000NodesRewire01.txt --type Edge  --output SmallWorld5000NodesRewire01
-python pruebasFractalidad.py --file ../datos/Generadas/SmallWorld5000NodesRewire02.txt --type Edge  --output SmallWorld5000NodesRewire02  
+python pruebasFractalidad.py --file ../datos/Generadas/SmallWorld5000NodesRewire01.txt --type Edge  --output SmallWorld5000NodesRewire01   OK
+python pruebasFractalidad.py --file ../datos/Generadas/SmallWorld5000NodesRewire02.txt --type Edge  --output SmallWorld5000NodesRewire02   OK
 
 #Maquina 4
-COMPLETA python pruebasFractalidadFaltantes.py --file ../datos/Generadas/Random1991Nodes5939.txt --type Edge --output Random1991Nodes5939  
-python pruebasFractalidad.py --file ../datos/Generadas/Random3373Nodes5978.txt --type Edge  --output Random3373Nodes5978
+python pruebasFractalidadFaltantes.py --file ../datos/Generadas/Random1991Nodes5939.txt --type Edge --output Random1991Nodes5939   COMPLETA
+python pruebasFractalidad.py --file ../datos/Generadas/Random3373Nodes5978.txt --type Edge  --output Random3373Nodes5978 OK
 
 #Maquina 5
-python pruebasFractalidad.py --file ../datos/Generadas/Random5620Nodes8804.txt --type Edge  --output Random5620Nodes8804
+python pruebasFractalidad.py --file ../datos/Generadas/Random5620Nodes8804.txt --type Edge  --output Random5620Nodes8804 OK
 python pruebasFractalidad.py --file ../datos/RedesReales/Celengs.net  --type Pajek --output Celengs  Haciendo faltantes
 
 #Maquina 6
-python pruebasFractalidadFaltantes.py --file ../datos/RedesReales/cerevisiae.net  --type Pajek --output cerevisiae Haciendo faltantes
-python pruebasFractalidad.py --file ../datos/RedesReales/EColi.net  --type Pajek --output EColi   Haciendo faltantes en 
+python pruebasFractalidadFaltantes.py --file ../datos/RedesReales/cerevisiae.net  --type Pajek --output cerevisiae Corrigiendo error Simulated Annealing SandBox
+python pruebasFractalidad.py --file ../datos/RedesReales/EColi.net  --type Pajek --output EColi   Haciendo faltantes en maquina 8
 
 #Maquina 7
-python pruebasFractalidad.py --file ../datos/Fractales/floweru2v2.net  --type Pajek --output floweru2v2
-python pruebasFractalidad.py --file ../datos/Fractales/floweru1v3.net  --type Pajek --output floweru1v3
+python pruebasFractalidad.py --file ../datos/Fractales/floweru2v2.net  --type Pajek --output floweru2v2  OK
+python pruebasFractalidad.py --file ../datos/Fractales/floweru1v3.net  --type Pajek --output floweru1v3  OK
 
 #Maquina 8
-python pruebasRobustez.py --file ../datos/Generadas/ScaleFree2000Nodes.txt --type Edge  --output ScaleFree2000Nodes  
+python pruebasRobustez.py --file ../datos/Generadas/ScaleFree2000Nodes.txt --type Edge  --output ScaleFree2000Nodes    
 python pruebasRobustez.py --file ../datos/Generadas/ScaleFree4000Nodes.txt --type Edge  --output ScaleFree4000Nodes  
 
 #Maquina 9
