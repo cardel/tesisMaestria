b9ca7d16a2bad856ce604f7e3534f38f3b79ba86
Your Name <you@example.com>
2018-07-05 00:08:01 -0500

    cambios

---
+++ implementacion/lamfria/Results/Graphics/article/robustnessAndFratality/generarArchivos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#for filename in 20*; do
+# 	echo $filename
+#    python createFile.py $filename
+#	python generarGraficas.py
+#done
+
+python createFile.py 20180512_143117ScaleFree8000Nodes
+python generarGraficas.py
